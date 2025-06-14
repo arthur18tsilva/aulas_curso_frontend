8203b87922e6c0db04924e88998b1e89b346e727
arthur18tsilva <arthurtavaressilva853@gmail.com>
2025-06-14 11:05:48 -0300

    Update script-04.js

--- script-04.js
+++ script-04.js
@@ -1,7 +1,8 @@
 function media(){
   var a = parseFloat(document.getElementById('valor01').value);
   var b = parseFloat(document.getElementById('valor02').value);
-  var m = (a+b)/2;
+  var c = parseFloat(document.getElementById('valor03').value);
+  var m = (a+b+c)/3;
 
   if(m == 10){
     document.getElementById('demo').innerHTML = "Média = "+ m.toString() + "(Aprovado com Distinção)";
