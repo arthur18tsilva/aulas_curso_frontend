b0599c1a79ae0787a5f0d44a5a4459297428316a
arthur18tsilva <arthurtavaressilva853@gmail.com>
2025-06-14 11:05:39 -0300

    Update script-03.js

--- script-03.js
+++ script-03.js
@@ -1,10 +1,14 @@
-function parImpar(){
+function maiorValor(){
   var a = parseInt(document.getElementById('valor01').value);
+  var b = parseInt(document.getElementById('valor02').value);
+  var c = parseInt(document.getElementById('valor03').value);
  
-  if(a % 2 == 0){
-    document.getElementById('demo').innerHTML = "É um número par.";
-  }else{
-    document.getElementById('demo').innerHTML = "É um número ímpar.";
+  if(a > b && a>c){
+    document.getElementById('demo').innerHTML = "(A) é o maior valor.";
+} else if(b > a && b>c){
+    document.getElementById('demo').innerHTML = "(B) é o maior valor.";
   }
-
+  else{
+    document.getElementById('demo').innerHTML = "(C) é o maior valor.";
+  }
 }
